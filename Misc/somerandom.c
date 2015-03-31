efe9bccbedd0dfb0957be988a11d0a5873db2581
Rajendra Kumar <rkumar2468@users.noreply.github.com>
2015-03-31 12:41:42 -0400

    Issue with the example
    {4, -2, -3, 4, -1, -2, 1, 5, -3};
    thanks to vinay for identifying

--- Misc/somerandom.c
+++ Misc/somerandom.c
@@ -3,27 +3,29 @@
 // Largest Sum contiguous array:
 int maxfun(int x, int y)
 {
-	return (x>y)?x:y;
+        return (x>y)?x:y;
 }
 
 int sum(int array[], int len)
 {
-	int max = array[0], maxsofar = array[0];
-	int count = 0;
-	while(count < len)
-	{
-		maxsofar = maxfun(array[count], maxsofar+array[count]);
-		max = (max < maxsofar)?maxsofar:max;
-		count++;
-	}
-	return max;
+        int max = array[0], maxsofar = 0;
+        int count = 0;
+        while(count < len)
+        {
+                maxsofar = maxfun(array[count], maxsofar+array[count]);
+                max = (max < maxsofar)?maxsofar:max;
+                count++;
+        }
+        return max;
 }
 int main()
 {
-	int a[] = {-2, -3, 4, -1, -2, 1, 5, -3};
-	int len = sizeof(a)/sizeof(int), i =0;
-	for(;i<len;i++)
-		printf(" %d ",a[i]);
-	printf("\nSum : %d\n", sum(a, len));
-	return 0;
+        // int a[] = {-2, -3, -4, -1, -2, -1, -5, -3};
+        // int a[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        int a[] =  {4, -2, -3, 4, -1, -2, 1, 5, -3};
+        int len = sizeof(a)/sizeof(int), i =0;
+        for(;i<len;i++)
+                printf(" %d ",a[i]);
+        printf("\nSum : %d\n", sum(a, len));
+        return 0;
 }
